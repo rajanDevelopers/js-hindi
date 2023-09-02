26ba6d91f8a576da2d647094ecd36cff425cbd00
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2023-09-02 15:42:28 +0000

    Datatype Basics in JavaScript

---
+++ 01_basics/01Basics.js
@@ -0,0 +1,17 @@
+const accountId = 623351; 
+let accountEmail = "rajan9523459381@gmail.com"
+var accountPassword = "1234"
+accountCity = "patna"
+
+// accountId = 2;
+console.log(accountId);
+
+accountEmail = "Goole@gmail.com"
+accountPassword = "623351"
+accountCity = "Bangluru"
+
+console.table([accountEmail,accountId,accountPassword, accountCity])
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/* Prefer not to use var because of issue in block issue  */
---
+++ 01_basics/02Data_type.js
@@ -0,0 +1,19 @@
+"Use straict"; // treat all JS code as newer version 
+
+// alert("Hello World") // we are using nodejs, not browser 
+
+// console.log(3 + 3 );
+
+// Type of DataType of JavaScript 
+
+// 1 Primitive DataType  2. Object DataType 
+
+// number  ==> 2 to power 53 
+// string ==> " "
+// boolean ==> true / false 
+// null ==> standalone value (Represtaion of the empty element )
+// undefined ==> Not Assigned the value 
+// symbol ==> unique 
+
+let LossThe = 100; 
+console.log(typeof LossThe);
