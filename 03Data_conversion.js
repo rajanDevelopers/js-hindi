690cb01c713b794024cdbbc6f684f9945a729b49
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2023-09-02 16:50:53 +0000

    Conversion class

---
+++ 03Data_conversion.js
@@ -0,0 +1,49 @@
+
+let score = 35;
+// console.log(typeof score);
+
+
+let Name = "Rajan Kumar Mahto"
+// consloe.log(typeof Name)
+
+// "33" ==> 33
+// "33abc" ==> NaN (Not a Number)
+// ture ==> 1; false ==> 0;
+// 
+
+
+let SomeNumber = 33;
+
+let stringName = String(SomeNumber)
+// console.log(stringName);
+// console.log(typeof StringName)
+
+let isLogged = 1;
+let LoggedForm = Boolean(isLogged)
+// console.log(LoggedForm);
+
+let Loggin = 0;
+let IsLoggin = Boolean(Loggin)
+// console.log(IsLoggin);
+
+// Note 
+
+// 0 ==> false ;
+// 1 ==> true ; 
+
+let sTringNumber = "100";
+
+let CheckData = Number(sTringNumber)
+// console.log( typeof CheckData);
+
+let EmptyString = ""
+let BooleanEmptyString = Boolean(EmptyString);
+// console.log(BooleanEmptyString);
+
+
+let NonEmptyString = "RajanKumar"
+let BooleanNonEmptyString = Boolean(NonEmptyString)
+console.log(BooleanNonEmptyString);
+
+// "" ==> false ;
+// "NonString" ==> true ;
