0614244e5caa54617c574b7734180f7751e6ab4a
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2023-09-12 16:37:41 +0000

    Date and Time in JavaSript

---
+++ 08_Date_Time.js
@@ -0,0 +1,31 @@
+// console.log("About More Details for Date and Time in javaScript ");
+
+// Date ==> Object 
+
+let MyDate = new Date();
+// console.log(MyDate);
+// console.log(MyDate.toString());
+
+// console.log(MyDate.toLocaleString());
+// console.log(MyDate.toDateString());
+// console.log(typeof MyDate)
+
+let myCreate = new Date();
+// console.log(myCreate.toDateString());
+// console.log(myCreate.getDate());
+// console.log(myCreate.getDay());
+// console.log(myCreate.getMonth());
+c// console.log(myCreate(Object.keys()));
+
+
+// Date new Formate 
+
+let CreatedDate = new Date(2023,10,12);
+console.log(CreatedDate.toLocaleString());
+
+
+
+
+
+
+
