221b35271547e2c50dae12583b2af6c5e8758016
RAJAN KUMAR MAHTO <you@example.com>
2023-09-26 22:53:21 +0530

    Day 03

---
+++ 04_Function/03_function.js
@@ -0,0 +1,97 @@
+// console.log("Day 3 Function Basics");
+
+// How to pass the under of function Multiple value 
+// ex aap mall jate hai to card ka user karke multiple goods ko purchage karte hai 
+// to js me ek method hota hai jo ki sabhi item ko ek bundle ke under store kar deta hai 
+
+
+// using rest Operator in under of Function 
+// function multipleCartValue (...item1) {
+
+//     return item1   
+    
+// }
+// console.log(multipleCartValue(100,500,400,900));
+
+
+
+// 
+function calculateCart(num1,num2,num3,num4,...num5) {
+    return num1,num2,num3,num4,num5
+
+}
+// console.log(calculateCart(10,100,600,700,152,960));
+
+
+// use to object under of Function 
+
+const student = {
+    name : 'Mohan Kumar', 
+    Reg_No : 620820104078, 
+    RollNO : '20cs109'
+}
+
+
+function  ObjectAccess(ObjectValueAccess) {
+    return ` Student Detail , 
+    Name : ${ObjectValueAccess.name},
+    Registor NO : ${ObjectValueAccess.Reg_No}, 
+    RollNo : ${ObjectValueAccess.RollNO}, 
+    Thank You `
+}
+// console.log(ObjectAccess(student));
+
+
+// Object use in the under of Function ==> Another Method Example 
+
+const StaffObject  = {
+    StaffName : "Sangita Mam", 
+    StaffId : 209, 
+    StaffSalary : 25000, 
+    StaffWork : '8h'
+}
+
+function Feculty (FecultyObjectValue ) {
+    return FecultyObjectValue
+}
+// console.log(Feculty(StaffObject));
+// Another method ==> 
+
+let UnderFunction = Feculty({
+    StaffName : "HItesh KUmar Chaudary", 
+    StaffId : 'CS1009', 
+    StaffSalary : '4000',
+    StaffWork : '6 Hour'
+
+})
+// console.log(UnderFunction)
+
+function Members(MembersObject) {
+     console.log(MembersObject);
+    // return MembersObject
+}
+ Members({
+    MembersName : "Rohini", 
+    MembersTotalFamily : 10, 
+    MembersLoaction : "Patna", 
+    MemberIncome : '500000 LPA'
+})
+
+
+// use the Array under of the Function 
+
+const MyArrayValue = [100, 178, 653, 253, 451, 365]
+
+function ArrayFunction(ArrayFunction) {
+
+    return ArrayFunction
+
+
+}
+
+console.log(ArrayFunction(MyArrayValue));
+
+
+
+
+
