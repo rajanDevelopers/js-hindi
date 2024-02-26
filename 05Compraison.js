c7bd0225fe3a08cf78d335a0026d6eac8673332d
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2024-02-26 10:16:25 +0530

    Update 05Compraison.js

--- 05Compraison.js
+++ 05Compraison.js
@@ -8,11 +8,11 @@
 
         // Ye sab Kunfsion 
 
-// console.log(null <= 0);
-// console.log(null == 0);
-// console.log(null >= 0);
+// console.log(null <= 0); ==> true
+// console.log(null == 0);  ==> false
+// console.log(null >= 0);  ==> true
 
 
-// console.log(undefined == 0);
-// console.log(undefined > 0);
-// console.log(undefined >= 0);
+// console.log(undefined == 0); ==> false
+// console.log(undefined > 0); ==> false
+// console.log(undefined >= 0); ==>false
