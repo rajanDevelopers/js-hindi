1ad5c41d40d5889c45993d6b0e65da7f5ca047c3
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2024-02-26 12:00:59 +0530

    Update 08_Date_TimeChange.js

--- 08_Date_Time.js
+++ 08_Date_Time.js
@@ -3,19 +3,39 @@
 // Date ==> Object
 
 let MyDate = new Date();
-// console.log(MyDate);
-// console.log(MyDate.toString());
-rf5tg6512;
-// console.log(MyDate.toLocaleString());
-// console.log(MyDate.toDateString());
-// console.log(typeof MyDate)
+console.log(MyDate);  // Mon Feb 26 2024 11:24:07 GMT+0530 (India Standard Time) // default toString() le leta hai ;
+console.log(MyDate.toString());  // Mon Feb 26 2024 11:24:07 GMT+0530 (India Standard Time)
+
+console.log(MyDate.toLocaleString()); //  2/26/2024, 11:24:07 AM
+console.log(MyDate.toDateString()); // 'Mon Feb 26 2024' ==> ye answer deta hai 
+console.log(typeof MyDate); // Object 
 
 let myCreate = new Date();
-// console.log(myCreate.toDateString());
-// console.log(myCreate.getDate());
-// console.log(myCreate.getDay());
-// console.log(myCreate.getMonth());
-// console.log(myCreate(Object.keys()));
+console.log(myCreate.toDateString());  
+console.log(myCreate.getDate()); // 26 ==> ye batata hai ki month ka koun sa date hai  
+
+let todaydate = new Date();
+console.log(todaydate.getDate()); // => ye aaj ka date batayega 
+
+
+console.log(myCreate.getDay()); // 1 ==> ye is week a kon sa day hai ye data hai 
+
+## The getDay() ==> method returns the day of the week (0 to 6) of a date.
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+console.log(day);
+
+
+console.log(myCreate.getMonth());
+
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const d = new Date();
+let name = month[d.getMonth()];
+
+
 
 // Date new Formate
 
@@ -33,7 +53,7 @@ let OldDate = new Date(2022, 3, 26);
 //));
 
 let MyTimeStamp = Date.now();
-c; //  onsole.log(MyTimeStamp);
+//  onsole.log(MyTimeStamp);
 
 // console.log(Date.now()/ 1000);
 // console.log(MyTimeStamp.sl);
