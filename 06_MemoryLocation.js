e509b2ffff96d5a4f79244b3841eff5ae195650e
rajanDevelopers <143403439+rajanDevelopers@users.noreply.github.com>
2024-02-26 10:31:12 +0530

    Update 06_MemoryLocation_Changes.js

--- 06_MemoryLocation.js
+++ 06_MemoryLocation.js
@@ -13,7 +13,14 @@ anothername = "chai and Code";
 
 // Exaple of The Heap Memory Location
 // Heap memory location me data ka Refrence Milta hai 
-// But Heap me data ka Copy milta hai  
+// But Stack me data ka Copy milta hai ==> jab bhi aap koi value change karte hai to wo orginal value me change nhi hota 
+let myYoutudeName = "hiteshchoudharydotcom"; 
+let anothername = myYoutudeName; // ==> yaha per anothername ko myYoutudeName ka copy mila hai 
+
+
+anothername = "chai and Code"; // ==> anothername ke under koi bhi changes karnege to wo only anothername ke hi under hoga ; 
+                                // na ki myyoutudename ke under 
+
 
 
 let userOne = {
@@ -24,9 +31,9 @@ let userOne = {
 
  console.log(userOne);
 
-let userTwo = userOne;
+let userTwo = userOne; // yaha per hamko diretly userone ka refernece mila hai to agar usertwo me jo bhi changes karnge to userOne me bhi hoga ;
 
-userTwo.email = "RajanKumar3210@gmail.com";
+userTwo.email = "RajanKumar3210@gmail.com"; // email userone ka bhi change ho gya hoga ;
 
 console.log(userOne.email);
 
